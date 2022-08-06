fc0a54e3c3454707b442b55fb09f6b33ce5942f1
CHJMCMXCIII <73843393+CHJMCMXCIII@users.noreply.github.com>
2022-08-06 20:36:58 +0900

    스타일 수정 및 가격에 콤마 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <h1 class="app-title">주가 모니터</h1>
+        <h1 class="app-title">주식 목표가격 모니터</h1>
         <div class="wrapper">
             <nav>
                 <ul>
@@ -11,6 +11,7 @@
                     <li @click="setData('SK이노베이션')" :class="{ 'active': name === 'SK이노베이션' }">SK이노베이션</li>
                 </ul>
             </nav>
+
             <div class="back" v-if="isLoading">
                 <div class="background"></div>
                 <div class="vs-loading">
@@ -24,23 +25,28 @@
             </div>
 
             <h2 class="stock-name">{{ name }}</h2>
-            <h3 class="stock-price-today">{{ displayingStockPrice }}</h3>
+            <h3 class="stock-price-today">{{ commaAddedTodayPrice }}<span>원</span></h3>
+            <div class="chart">
+                <svg></svg>
+            </div>
             <div class="target-price">
-                <label for="target" v-if="parseInt(targetPrice) === 0">목표 매수금액을 설정하세요!</label>
-                <label for="target" v-else-if="parseInt(targetPrice) !== 0">아래 버튼을 눌러 저장해주세요!</label>
-                <label for="target" v-else>저장되었습니다.</label>
-                <input id="target" v-model.number="targetPrice" type="number" step="500">
+                <label for="target" v-if="parseInt(targetPrice) === 0">목표 매수금액을 설정하세요.</label>
+                <label for="target" v-else-if="parseInt(targetPrice) !== 0 && isSaved === false">아래 버튼을 눌러 목표 매수가를 저장해주세요.</label>
+                <label for="target" v-else-if="parseInt(targetPrice) !== 0 && isSaved === true">목표 매수가가 저장됐어요.</label>
+                <input id="target" v-model.number="targetPrice" type="number" step="500" @click="isSaved = false">
                 <button @click="setTargetPrice()">
                     저장
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path
                             d="M15.563 22.282l-3.563.718.72-3.562 2.843 2.844zm-2.137-3.552l2.845 2.845 7.729-7.73-2.845-2.845-7.729 7.73zm-3.062 2.27h-7.364v-7h12.327l6.673-6.688v-2.312l-4-4h-18v22h9.953l.411-2zm-5.364-18h12v7h-12v-7zm8.004 6h2.996v-5h-2.996v5z" />
-                    </svg></button>
+                    </svg>
+                </button>
             </div>
         </div>
     </section>
 </template>
 <script>
+import * as d3 from 'd3'
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
@@ -54,23 +60,96 @@ export default {
     // template 에서 해당 변수와 함수를 접근할수있다.
     setup() {
         let name = ref("삼성전자")
+        let message = ref("")
         let isLoading = ref(true)
         let targetPrice = ref(0)
+        let isSaved = ref(false)
         let displayingStockPrice = ref({})
         let displayingStockPriceList = ref({})
         let stockPriceToday = {}
         let stockPriceList = {}
 
+        const draw = (target, now) => {
+            d3.select(".chart svg").selectAll("g").remove()
+            const remain = ((now - Math.max(now - target, 0)) / now) * 100
+            if (remain === 100) {
+                message.value = `살 때가 왔군요!`
+            } else if (remain >= 50) {
+                message.value = `${Math.round(remain)}% 네요 조금만 참으세요.`
+            } else {
+                message.value = `${Math.round(remain)}% 입니다. 장기적으로 바라봐요.`
+            }
+
+            const width = 260
+            const height = 260
+            const radius = Math.min(width, height) / 2.3
+            const group = d3.select(".chart svg")
+                        .attr("width", width)
+                        .attr("height", height)
+                        .append("g")
+                        .attr("transform", `translate(${width / 2}, ${height / 2})`)
+
+            const pieGenerator = d3.pie().sort(null)
+
+            const arc = d3.arc()
+                        .innerRadius(radius * 0.9)
+                        .outerRadius(radius)
+            
+            const textDOM = group.append("text")
+                        .attr("text-anchor", "middle")
+                        .attr("dy", ".4em")
+                        .attr("font-size", "6.4rem")
+                        .attr("font-weight", "bold")
+                        .style("fill", "#7f00ff")
+
+            group.append("text")
+                .attr("text-anchor", "middle")
+                .attr("dy", "-2em")
+                .attr("font-size", "1.4rem")
+                .text("목표까지")
+                .style("fill", "#333")
+                .style("font-weight", "500")
+
+            group.append("path")
+                .data(pieGenerator([1]))
+                .attr("class", "backColor")
+                .attr("d", arc)
+
+            const foreground = group.append("path")
+                .data(pieGenerator([0, 100]))
+                .attr("class", (d, i) => `frontColor${i}`)
+                .attr("d", arc)
+
+            const format = d3.format(".0%")
+
+            function arcTween(pie) {
+                return function (d) {
+                const interpolate = d3.interpolate(pie[0].startAngle, pie[0].endAngle)
+                const interpolateText = d3.interpolate(0, pie[0].value)
+                return function (t) {
+                    d.endAngle = interpolate(t)
+                    textDOM.text(format(interpolateText(t) / 100))
+                    return arc(d)
+                }
+                }
+            }
+            foreground.transition()
+                .duration(1500)
+                .attrTween("d", arcTween(pieGenerator([remain, 100 - remain])))
+                .delay(1000)
+        }
+
         const setTargetPrice = () => {
             setLocalStorage(name.value, targetPrice.value)
+            isSaved.value = true
         }
 
         const getLocalStorage = name => {
             return localStorage.getItem(name) || 0
         }
 
-        const setLocalStorage = (name, value) => {
-            localStorage.setItem(name, value)
+        const setLocalStorage = (name, value, saved) => {
+            localStorage.setItem(name, value, saved)
         }
 
         let setData = (data) => {
@@ -78,6 +157,7 @@ export default {
             displayingStockPrice.value = stockPriceToday[data]
             displayingStockPriceList.value = stockPriceList[data]
             targetPrice.value = getLocalStorage(data)
+            draw(targetPrice.value, displayingStockPrice.value)
         }
 
 
@@ -100,11 +180,13 @@ export default {
                 .then(res => {
                     stockPriceToday = res.data
                     displayingStockPrice.value = stockPriceToday[name.value]
+                    draw(targetPrice.value, displayingStockPrice.value)
                 })
         }, 1000 * 60)
 
         return {
             isLoading,
+            isSaved,
             targetPrice,
             stockPriceToday,
             stockPriceList,
@@ -116,6 +198,11 @@ export default {
 
 
 
+    },
+    computed: {
+        commaAddedTodayPrice() {
+            return this.displayingStockPrice.toLocaleString('ko-KR');
+        }
     }
 }
 </script>
@@ -165,7 +252,7 @@ nav {
 
     label {
         display: inline-block;
-        margin-left: .8rem;
+        margin-left: .4rem;
         color: #ddd;
         font-size: 1.4rem;
     }
@@ -179,6 +266,7 @@ nav {
         background: #fff;
         color: #333;
         font-size: 2.4rem;
+        font-weight: 500;
     }
 
     button {
@@ -186,6 +274,7 @@ nav {
         padding: 1.8rem;
         background-color: #7f00ff;
         text-align: center;
+        color: #fff;
         font-size: 1.8rem;
         font-weight: 500;
         transition: background-color .2s ease-out, color .2s ease-out;
@@ -204,6 +293,23 @@ nav {
                 fill: #1f2023;
             }
         }
+    }
+}
+
+.chart {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.6rem 0;
+    margin: 0 0 2.4rem 0;
+    background-color: #e3e3ff;
+    border-radius: 100%;
+
+    .backColor {
+        fill: #aaa;
+    }
+    .frontColor0 {
+        fill: #7f00ff;
     }
 }
 </style>
